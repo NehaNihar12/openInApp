14b10b93637f7fbdf1caba258158215dd8d89e6c
Neha Nihar <clearglass207@gmail.com>
2024-01-29 12:39:22 +0530

    Changed spinner colour

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -37,7 +37,7 @@ const Button = ({
         <div role="status">
           <svg
             aria-hidden="true"
-            class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-white-primary"
+            class="w-8 h-8 text-white-primary animate-spin  fill-blue-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
